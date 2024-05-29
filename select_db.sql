449850aa50c65c8a28640a15c44dcf7c34f3ab98
oskarwilms1 <149292696+oskarwilms1@users.noreply.github.com>
2024-05-29 19:53:39 +0200

    Update select_db.sql

--- select_db.sql
+++ select_db.sql
@@ -24,6 +24,16 @@ select nazwa_produktu from produkt inner join typ_produktu on produkt.id_typu_pr
 select nazwa_produktu from produkt inner join typ_produktu on produkt.id_typu_prod = typ_produktu.id_typu_prod where typ_produktu.nazwa_typu like 'Wino%';
 /*Wyświetl wszystkich klientów, którzy nic nie kupili*/
 select id_klienta,nazwa_klienta from klient WHERE NOT EXISTS (SELECT 1 from sprzedaż WHERE klient.id_klienta = sprzedaż.id_klienta);
+/*Wyświetl te Kraje, z których mamy klientów,ale nie mamy z nich producentów*/
+select ID_Kraju from klient where klient.ID_Kraju not in (select ID_Kraju from producent);
+/*Stwórz widok Dochód*/
+CREATE VIEW Dochód as select waluta, SUM(ilość*cena_za_sztuke) as przychód from sprzedaż group by waluta;
+/*Operacja na widoku*/
+select waluta from dochód where waluta = 'PLN' or waluta = 'USD';
+/*Usuń klientów, którzy nie dokonali żadnej transakcji*/
+DELETE FROM klient WHERE NOT EXISTS (SELECT 1 from sprzedaż WHERE klient.id_klienta = sprzedaż.id_klienta);
+
+
 
 
 
