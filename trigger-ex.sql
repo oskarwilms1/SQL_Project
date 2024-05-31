0f74ad05f2d6d3ebe5467a794088757909481437
oskarwilms1 <149292696+oskarwilms1@users.noreply.github.com>
2024-05-31 18:45:49 +0200

    Update trigger-ex.sql

--- trigger-ex.sql
+++ trigger-ex.sql
@@ -1,2 +1,4 @@
 /*sprawdzam czy mogę dodać ujemną cenę na produkcie*/
 insert into sprzedaż(id_produktu,id_klienta,ilość,cena_za_sztuke,waluta,data_sprzedaży) values (7771, 1003,10,-10.00,'USD','2024-06-15 10:00:00');
+/*sprawdzam czy mogę dodać datę większą niż 'dzisiaj'*/
+INSERT INTO sprzedaż(id_produktu,id_klienta,ilość,cena_za_sztuke,waluta,data_sprzedaży) VALUES (7774,1005,2,100.00,'PLN','2040-08-17 11:00:00');
