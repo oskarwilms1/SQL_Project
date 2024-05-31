989c38f33e75d3f5d6742522a12d8723cd1f598f
oskarwilms1 <149292696+oskarwilms1@users.noreply.github.com>
2024-05-31 18:39:56 +0200

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,4 +1,10 @@
 CREATE TRIGGER on_insert_update_sprzedaż
 BEFORE UPDATE OR INSERT ON sprzedaż
 FOR EACH ROW
-EXECUTE FUNCTION check_if_negative();
+EXECUTE PROCEDURE check_if_negative();
+
+
+CREATE TRIGGER on_insert_sprzedaż
+BEFORE INSERT ON sprzedaż
+FOR EACH ROW
+EXECUTE PROCEDURE check_if_correct_date();
