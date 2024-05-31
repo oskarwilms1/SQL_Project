41d10ccaa78efb770e20f15570955bacef19cef3
oskarwilms1 <149292696+oskarwilms1@users.noreply.github.com>
2024-05-31 17:41:34 +0200

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -2,7 +2,9 @@ CREATE OR REPLACE FUNCTION check_if_negative()
 RETURNS TRIGGER AS $$
 BEGIN
     IF (NEW.cena_za_sztuke <= 0) THEN
-    RETURN NEW;
+        RETURN NULL;
+    ELSE 
+        RETURN NEW;
   END IF;
 END;
 $$ LANGUAGE plpgsql;
